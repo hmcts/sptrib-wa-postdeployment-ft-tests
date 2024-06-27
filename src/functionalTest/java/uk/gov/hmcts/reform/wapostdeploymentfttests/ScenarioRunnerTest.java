84ecae4e3d6f5ef3d88a7b42b2a3f193cd2a6bd1
gmmagruder <gabrielle.magruder@gmail.com>
2024-06-27 13:09:14 +0100

    Try ignore

--- src/functionalTest/java/uk/gov/hmcts/reform/wapostdeploymentfttests/ScenarioRunnerTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/wapostdeploymentfttests/ScenarioRunnerTest.java
@@ -7,8 +7,8 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.awaitility.core.ConditionEvaluationLogger;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.jupiter.api.Disabled;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.AbstractEnvironment;
@@ -119,7 +119,7 @@ public class ScenarioRunnerTest extends SpringBootFunctionalBaseTest {
     }
 
     @Test
-    @Disabled
+    @Ignore
     public void scenarios_should_behave_as_specified() throws Exception {
 
         StopWatch stopWatch = new StopWatch();
